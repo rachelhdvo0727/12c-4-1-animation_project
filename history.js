c18c2136832c4e23b67bdae0ad6226169dffc119
rachelvo <rach0727@stud.kea.dk>
2020-03-23 18:50:02 +0100

    changed class names in svg

--- history.js
+++ history.js
@@ -1,17 +1,43 @@
 "use strict";
 export function historyGlobalSVGs() {
+  popUpWindow();
   popUpLight();
-demonSVG();
+  demonSVG();
 
+  function popUpWindow() {
+    let popup = document.createElementNS("http://www.w3.org/2000/svg", "use");
+    popup.setAttribute("href", "#globalPopUp");
+
+    popup.setAttribute("x", "1845px");
+    popup.setAttribute("y", "100px");
+    popup.setAttribute("height", "500px");
+    popup.setAttribute("width", "700px");
+
+    document.querySelector("#history-svg-bg").appendChild(popup);
+  }
   function popUpLight() {
-    let use = document.createElementNS("http://www.w3.org/2000/svg", "use");
-    use.setAttribute("href", "#globalLightBulb");
+    let lightbulb = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "use"
+    );
+    lightbulb.setAttribute("href", "#globalLightBulb");
 
-    use.setAttribute("x", "1750px");
-    use.setAttribute("y", "300px");
-    use.setAttribute("height", "130px");
-    use.setAttribute("width", "130px");
+    lightbulb.setAttribute("x", "1800px");
+    lightbulb.setAttribute("y", "400px");
+    lightbulb.setAttribute("height", "130px");
+    lightbulb.setAttribute("width", "130px");
 
-    document.querySelector("#history-svg-bg").appendChild(use);
+    document.querySelector("#history-svg-bg").appendChild(lightbulb);
+  }
+  function demonSVG() {
+    let demon = document.createElementNS("http://www.w3.org/2000/svg", "use");
+    demon.setAttribute("href", "#demon-svg");
+
+    demon.setAttribute("x", "1800px");
+    demon.setAttribute("y", "700px");
+    demon.setAttribute("height", "130px");
+    demon.setAttribute("width", "130px");
+
+    document.querySelector("#history-svg-bg").appendChild(demon);
   }
 }
