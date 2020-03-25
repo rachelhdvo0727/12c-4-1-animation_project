4dc1eaf5359ef6384941b71acdd1d2239c7a4783
rachelvo <rach0727@stud.kea.dk>
2020-03-25 20:10:41 +0100

    rykkede den første lampe

--- history.js
+++ history.js
@@ -4,6 +4,10 @@
 import { sendData } from "./script";
 
 export function historyGlobalSVGs() {
+  //save data fra script.js
+  let data = sendData();
+  let selected = [];
+
   createModalsandLights();
   edisonSvg();
   demonSvg();
@@ -12,13 +16,13 @@ export function historyGlobalSVGs() {
   function createModalsandLights() {
     for (let i = 0; i < 4; i++) {
       console.log("create Modals and Lights");
-      popUpWindow();
-      popUpLight();
+      popUpSVGs();
+      lightbulbSVGs();
     }
     placeModalsandLights();
     clickLightBulb();
   }
-  function popUpLight() {
+  function lightbulbSVGs() {
     let lightbulb = document.createElementNS(
       "http://www.w3.org/2000/svg",
       "use"
@@ -31,129 +35,132 @@ export function historyGlobalSVGs() {
     document.querySelector("#history-svg-bg").appendChild(lightbulb);
   }
 
-  function popUpWindow() {
+  function popUpSVGs() {
     let popup = document.createElementNS("http://www.w3.org/2000/svg", "use");
     popup.setAttribute("href", "#globalPopUp");
 
     popup.setAttribute("height", "500px");
     popup.setAttribute("width", "700px");
-    //popup.classList.add("hide");
+    popup.classList.add("hide");
 
     document.querySelector("#history-svg-bg").appendChild(popup);
   }
 
+  function createTexttags() {
+    console.log("showModalsText");
+    let textSvg = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "text"
+    );
+    textSvg.setAttribute("x", "80px");
+    textSvg.setAttribute("y", "100px");
+    textSvg.setAttribute("transform", "translate(88.6 111.65)");
+    textSvg.setAttribute("font-size", "10px");
+    textSvg.classList.add("theText");
+
+    document.querySelector("#globalPopUp > #Layer_2").appendChild(textSvg);
+  }
+
   function placeModalsandLights() {
     console.log("placing modals and lights");
-    //all lights
+    selected = [
+      {
+        firesPopup: document.querySelector(
+          "#history-svg-bg > use:nth-child(4)"
+        ),
+        firesLightbulb: document.querySelector(
+          "#history-svg-bg>use:nth-child(5)"
+        )
+      },
+      {
+        edisonsPopup: document.querySelector(
+          "#history-svg-bg > use:nth-child(6)"
+        ),
+        edisonsLightbulb: document.querySelector(
+          "#history-svg-bg > use:nth-child(7)"
+        )
+      },
+      {
+        monstersPopup: document.querySelector(
+          "#history-svg-bg > use:nth-child(8)"
+        ),
+        monstersLightbulb: document.querySelector(
+          "#history-svg-bg > use:nth-child(9)"
+        )
+      },
+      {
+        firstlampsPopup: document.querySelector(
+          "#history-svg-bg > use:nth-child(10)"
+        ),
+        firstlampsLightbulb: document.querySelector(
+          "#history-svg-bg>use:nth-child(11)"
+        )
+      }
+    ];
+
     //fire
-    document
-      .querySelector("#history-svg-bg>use:nth-child(5)")
-      .setAttribute("x", "1910px");
-    document
-      .querySelector("#history-svg-bg > use:nth-child(5)")
-      .setAttribute("y", "450px");
+    selected[0].firesLightbulb.setAttribute("x", "1910px");
+    selected[0].firesLightbulb.setAttribute("y", "450px");
+    selected[0].firesPopup.setAttribute("x", "1945px");
+    selected[0].firesPopup.setAttribute("y", "100px");
+
     //edison
-    document
-      .querySelector("#history-svg-bg > use:nth-child(7)")
-      .setAttribute("x", "568px");
-    document
-      .querySelector("#history-svg-bg > use:nth-child(7)")
-      .setAttribute("y", "1650px");
+    selected[1].edisonsLightbulb.setAttribute("x", "568px");
+    selected[1].edisonsLightbulb.setAttribute("y", "1650px");
+    selected[1].edisonsPopup.setAttribute("x", "600px");
+    selected[1].edisonsPopup.setAttribute("y", "1300px");
 
     //monster
-    document
-      .querySelector("#history-svg-bg > use:nth-child(9)")
-      .setAttribute("x", "910px");
-    document
-      .querySelector("#history-svg-bg > use:nth-child(9)")
-      .setAttribute("y", "1045px");
+    selected[2].monstersLightbulb.setAttribute("x", "910px");
+    selected[2].monstersLightbulb.setAttribute("y", "1045px");
+    selected[2].monstersPopup.setAttribute("x", "950px");
+    selected[2].monstersPopup.setAttribute("y", "700px");
 
     //the first lamp
-    document
-      .querySelector("#history-svg-bg > use:nth-child(11)")
-      .setAttribute("x", "2120px");
-    document
-      .querySelector("#history-svg-bg > use:nth-child(11)")
-      .setAttribute("y", "1350px");
-
-    //all modals
-    document
-      .querySelector("#history-svg-bg > use:nth-child(4)")
-      .setAttribute("x", "1945px");
-    document
-      .querySelector("#history-svg-bg > use:nth-child(4)")
-      .setAttribute("y", "100px");
-    document
-      .querySelector("#history-svg-bg > use:nth-child(6)")
-      .setAttribute("x", "600px");
-    document
-      .querySelector("#history-svg-bg > use:nth-child(6)")
-      .setAttribute("y", "1300px");
-    document
-      .querySelector("#history-svg-bg > use:nth-child(8)")
-      .setAttribute("x", "950px");
-    document
-      .querySelector("#history-svg-bg > use:nth-child(8)")
-      .setAttribute("y", "700px");
-
-    //the first lamp
-    document
-      .querySelector("#history-svg-bg > use:nth-child(10)")
-      .setAttribute("x", "2150px");
-    document
-      .querySelector("#history-svg-bg > use:nth-child(10)")
-      .setAttribute("y", "1000px");
+    selected[3].firstlampsLightbulb.setAttribute("x", "1900px");
+    selected[3].firstlampsLightbulb.setAttribute("y", "1350px");
+    selected[3].firstlampsPopup.setAttribute("x", "2000px");
+    selected[3].firstlampsPopup.setAttribute("y", "1000px");
   }
 
   function clickLightBulb() {
     console.log("clickLightBulb");
+
+    createTexttags();
     document
       .querySelectorAll(
         "use:nth-child(5), use:nth-child(7), use:nth-child(9), use:nth-child(11)"
       )
       .forEach(light => {
-        console.log("events");
         light.addEventListener("click", function() {
-          showModals();
           console.log("clicked");
           light.classList.toggle("clone");
+
+          if (light === selected[0].firesLightbulb) {
+            selected[0].firesPopup.classList.toggle("hide");
+            document.querySelector("#globalPopUp > g > text").innerHTML =
+              data[1].popup1;
+            wrapSVGsText();
+          } else if (light === selected[1].edisonsLightbulb) {
+            selected[1].edisonsPopup.classList.toggle("hide");
+            document.querySelector("#globalPopUp > g > text").innerHTML =
+              data[1].popup3;
+            wrapSVGsText();
+          } else if (light === selected[2].monstersLightbulb) {
+            selected[2].monstersPopup.classList.toggle("hide");
+            document.querySelector("#globalPopUp > g > text").innerHTML =
+              data[1].popup2;
+            wrapSVGsText();
+          } else if (light === selected[3].firstlampsLightbulb) {
+            selected[3].firstlampsPopup.classList.toggle("hide");
+            document.querySelector("#globalPopUp > g > text").innerHTML =
+              data[1].popup4;
+            wrapSVGsText();
+          }
         });
       });
   }
-
-  function showModals() {
-    document
-      .querySelectorAll(
-        "use:nth-child(5), use:nth-child(7), use:nth-child(9), use:nth-child(11)"
-      )
-      .forEach(elm => {
-        console.log("toggle");
-        elm.classList.toggle("hide");
-      });
-  }
-  function showModalsText() {
-    console.log("showModalsText");
-
-    //save data fra script.js
-    let data = sendData();
-    console.log(data);
-
-    //create a <text> in svg
-    let textSvg = document.createElementNS(
-      "http://www.w3.org/2000/svg",
-      "text"
-    );
-
-    textSvg.setAttribute("transform", "translate(88.6 111.65)");
-    textSvg.setAttribute("text-anchor", "start");
-    textSvg.classList.add("theText");
-
-    document.querySelector("#globalPopUp > #Layer_2").appendChild(textSvg);
-
-    //append text in svg
-    data.forEach(text => {
-      textSvg.innerHTML = `${text.popup1}`;
-    });
+  function wrapSVGsText() {
     d3plus
       .textwrap()
       .container(d3.select(".theText"))
@@ -164,7 +171,6 @@ export function historyGlobalSVGs() {
       .resize(true)
       .draw();
   }
-
   function edisonSvg() {
     let figure = document.createElementNS("http://www.w3.org/2000/svg", "use");
     figure.setAttributeNS(
@@ -205,7 +211,7 @@ export function historyGlobalSVGs() {
       "svg/paere-ny.svg#firstlamp"
     );
 
-    lamp.setAttributeNS(null, "x", "1700px");
+    lamp.setAttributeNS(null, "x", "1500px");
     lamp.setAttributeNS(null, "y", "1000px");
     lamp.setAttributeNS(null, "height", "500px");
     lamp.setAttributeNS(null, "width", "500px");
