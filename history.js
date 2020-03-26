1d76156ee05187ae66c5eb7bab8d79c221408fad
rachelvo <rach0727@stud.kea.dk>
2020-03-26 16:29:40 +0100

    fixed edison-ny.svg

--- history.js
+++ history.js
@@ -270,13 +270,15 @@ export function historyGlobalSVGs() {
 
   function edisonSvg() {
     let figure = document.createElementNS("http://www.w3.org/2000/svg", "use");
-    figure.setAttribute("href", "#edisonsvg");
-
-    figure.setAttribute("x", "150px");
-    figure.setAttribute("y", "1400px");
-    figure.setAttribute("height", "500px");
-    figure.setAttribute("width", "500px");
-
+    figure.setAttributeNS(
+      "http://www.w3.org/1999/xlink",
+      "xlink:href",
+      "svg/edison-ny.svg#edison"
+    );
+    figure.setAttributeNS("http://www.w3.org/1999/xlink", "x", "150px");
+    figure.setAttributeNS("http://www.w3.org/1999/xlink", "y", "1400px");
+    figure.setAttributeNS("http://www.w3.org/1999/xlink", "height", "500px");
+    figure.setAttributeNS("http://www.w3.org/1999/xlink", "width", "500px");
     document.querySelector("#history-svg-bg").appendChild(figure);
     console.log(figure);
   }
